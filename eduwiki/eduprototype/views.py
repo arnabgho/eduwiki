7b0a9d456d7d8de9cbe3dadf24bdb3520b8f862e
Androooou <asherman50@gmail.com>
2014-06-26 15:18:59 -0400

    Implemented simple learn page
    To be made more understandable soon but for now spits out the
    descriptions of things the user was incorrect on, as well as the
    description of the main topic

--- eduwiki/eduprototype/views.py
+++ eduwiki/eduprototype/views.py
@@ -21,18 +21,20 @@ def index(request):
     return render_to_response('eduprototype/index.html', context_dict, context)
 
 #the intro view
-def intro(request, context_dict_in={}):
+def intro(request):
     if not 'q' in request.GET or not request.GET['q']:
         return render_to_response('eduprototype/index.html')
-    search_name_url = request.GET['q']
+    searchterm = request.GET['q']
+    context_dict = {}
     context = RequestContext(request)
-    context_dict = context_dict_in
-    json_tree = query(search_name_url)  #comes out in a JSON string
+    json_tree = query(searchterm)  #comes out in a JSON string
     tree = json.loads(json_tree, object_hook=recurhook)  #converted into a python dictionary
+    request.session['tree'] = tree
     context_dict['quiz'] = make_quiz(tree)
     context_dict['tree'] = tree          #save it to context
     return render_to_response('eduprototype/intro.html', context_dict, context)
 
+#this is a test view now, probably best idea is to rename intro to quiz
 def quiz(request):
     context = RequestContext(request)
     #tree = request.session['tree']
@@ -40,10 +42,32 @@ def quiz(request):
     context_dict = {}
     return render_to_response('eduprototype/quiz.html', context_dict, context)
 
+#the learn view, this is displayed after the quiz is completed to display
+#the information the user needs
+def learn(request):
+    context = RequestContext(request)
+    if not 'q0' in request.GET or not request.GET['q0']:
+        return render_to_response('eduprototype/index.html')
+    
+    #get a list of which questions had correct responses
+    responses = quiz_correctness(request)
+    
+    #note: this is weird because it pulls from session vars, maybe fix later
+    #other note: it's also kinda hard-codey
+    tree = request.session['tree']
+    #get the topics of the main subject
+    main_topic = tree['text']
+    subtopics = [child if not correct else None for child, correct in zip(tree['children'], responses)]
+    
+    context_dict = { 'tree' : tree, "subtopics" : subtopics }
+    return render_to_response('eduprototype/learn.html', context_dict, context)
+    
+
 
 #code for assembling the dicts needed for the quizzes
 
 def make_quiz(topic):
+    #goes though the quizzes and returns a list of dicts for the quiz page
     children = topic['children']
     quiz = []
     for i, child in enumerate(children):
@@ -60,13 +84,29 @@ def make_question(topic, num):
     l = len(answers_prerand)
     found = False
     for i in range(l):
-        correct = "false"
+        correct = "incorrect"
         rand = randint(0, len(answers_prerand) - 1)
         if rand == 0 and found == False:
-            correct = "true"
+            correct = "correct"
             found = True
         answers.append({ 'text' : answers_prerand.pop(rand), 'correct' : correct })
     return { 'title': topic['title'], 'number': num, 'answers' : answers }
+
+
+#really ugly code to take the request to learn and get back a list
+#of booleans representing whether or not the user answered correctly
+def quiz_correctness(request):
+    i = 0
+    while ('q'+str(i)) in request.GET:
+        i += 1
+    responses_raw = [request.GET['q'+str(j)] for j in range(i)]
+    responses = []
+    for response in responses_raw:
+        if response == "correct":
+            responses.append(True)
+        else:
+            responses.append(False)
+    return responses
 
 
 #extra code to fix issues with the way json.loads processes this
