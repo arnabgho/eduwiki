697e78de8b509d6c9873ed221cf92e775ef77c77
Androooou <asherman50@gmail.com>
2014-06-29 01:26:03 -0400

    Better README and small PEP8 fixes

---
+++ eduwiki/eduprototype/diagnose/diagnose.py
@@ -0,0 +1,189 @@
+import os, tempfile, time, hashlib
+import wikipedia
+import re
+import json
+import sys
+import unicodedata
+
+
+
+#takes a search term and, optionally, a depth and branch factor, and returns a JSON tree
+#representing the term's article, with its prereqs and quiz items as well as those of 
+#the prereqs, and of their prereqs, etc.
+def query(search_term, depth = 1, children = 3):
+    #set a max depth and branching factor as a failsafe
+    if depth > 6:
+        depth = 6
+    if children > 6:
+        children = 6
+    
+    #get the topic and the names of its prereq links
+    main_topic = WikiEducate(normal(search_term))
+    prereqs = main_topic.wikilinks(children)
+    topic_name = main_topic.page.title
+    
+    #create a JSON tree which will be recursively built
+    json_children = []
+    
+    #get topic text, descriptor, and distractors
+    topic_text = main_topic.plainTextSummary(1)
+    description = main_topic.returnWhatIs() #note: I'm referring to the actual
+    distractor_names = main_topic.wikilinks(children)    #string of text as the distractor
+    distractors = []
+    for i in range(0,3):
+        distractor_name = normal(distractor_names[i])
+        distractor_obj = WikiEducate(distractor_name)
+        distractor = distractor_obj.returnWhatIs()    
+        distractors.append(distractor) #append dis tractor *vroom*
+    
+    #run for children if depth left
+    if depth != 0:
+        for j in range(0,children):
+            json_child = query(prereqs[j], depth = depth - 1, children = children)
+            json_children.append(json_child)
+    
+    #assemble the tree and return it
+    json_tree = {'title': topic_name, 'text': topic_text,
+                      'description': description, 'distractors': distractors,
+                      'children': json_children}
+    return json.dumps(json_tree)
+
+def normal(text):
+    try:
+        return unicodedata.normalize('NFKD', text).encode('ascii')
+    except UnicodeEncodeError as u:
+        return "ascii"
+
+def getpage(topic):
+    return WikiEducate(topic)
+
+def getpage_exact(topic):
+    return WikiEducate(topic, autosuggest=False)
+
+class WikiEducate:
+    def __init__(self, topic, cache=True, autosuggest=True):
+        self.topic = topic
+        self.cache = cache
+        self.fetcher = DiskCacheFetcher('url_cache')
+        self.page = wikipedia.page(self.topic, auto_suggest=autosuggest)
+
+    
+    def wikitext(self):
+        text = self.page.wikitext()
+        return text
+
+    def wikilinks(self, num):
+        wtext = self.wikitext()
+        #print wtext
+        wikilink_rx = re.compile(r'\[\[([^|\]]*\|)?([^\]]+)\]\]')
+        link_array = []
+        for m in wikilink_rx.finditer(wtext):
+            if len(link_array) > num:
+                break
+            if m.group(1) is not None:
+                if "Image" in m.group(1) or "Template" in m.group(1) or "File" in m.group(1):
+                    continue
+                link_array.append(m.group(1)[:-1])
+            else:
+                if "Image" in m.group(2) or "Template" in m.group(2) or "File" in m.group(2):
+                    continue
+                link_array.append(m.group(2))
+        return link_array
+
+
+    # Returns (up to) first n paragraphs of given Wikipedia article.
+    def plainTextSummary(self, n=2):
+        cached = self.cache and self.fetcher.fetch(self.topic+"-plainTextSummary")
+        if cached:
+            page_content = cached
+        else:
+            self.page = wikipedia.page(self.topic)
+            page_content = self.page.content
+            self.fetcher.cache(self.topic+"-plainTextSummary", page_content)
+        first_n_paragraphs = "\n".join(page_content.split("\n")[:n])
+        return first_n_paragraphs
+
+    # Returns an array of article titles for wiki links within given wiki text.
+    def topWikiLinks(self, n=2):
+        cached = self.cache and self.fetcher.fetch(self.topic+"-links")
+        if cached:
+            links = json.loads(cached)
+        else:
+            self.page = wikipedia.page(self.topic)
+            links = self.page.links
+            self.fetcher.cache(self.topic+"-links", json.dumps(links))
+        return links
+
+    # Returns an array of category titles
+    def categoryTitles(self):
+        cached = self.cache and self.fetcher.fetch(self.topic+"-categories")
+        if cached:
+            categories = json.loads(cached)
+        else:
+            self.page = wikipedia.page(self.topic)
+            categories = self.page.categories
+            self.fetcher.cache(self.topic+"-categories", json.dumps(categories))
+        return categories
+
+    # Returns first mention in article of the following regex
+    # "<topic>\s[^\.](is|was)([^\.])+\." or None (if no matches)
+    def returnWhatIs(self):
+        cached = self.cache and self.fetcher.fetch(self.topic+"-whatis")
+        if cached:
+            whatis = cached
+        else:
+            self.page = wikipedia.page(self.topic)
+            #print self.topic + "||" + self.topic[:7] + "||" + self.page.content[:200] + "\n"
+            regex_str = '('+self.topic[:5]+'('+self.topic[5:]+')?'+'|'+'('+self.topic[:len(self.topic)-5]+')?'+self.topic[len(self.topic)-5:] + ')' + '(\s[^\.]*(is|was|can be regarded as)|[^,\.]{,15}?,)\s([^\.]+)\.(?=\s)'
+            #print regex_str + "\n"
+            mentions = re.findall(regex_str, self.page.content, re.IGNORECASE)
+
+            if len(mentions) > 0:
+                whatis = mentions[0][5]
+                whatis = re.sub(r'.*\sis\s+(.*)$', r'\1', whatis)
+            else:
+                whatis = "can't find a good description"
+            self.fetcher.cache(self.topic+"-whatis", whatis)
+        return whatis
+
+
+
+
+
+
+class DiskCacheFetcher:
+    def __init__(self, cache_dir=None):
+        # If no cache directory specified, use system temp directory
+        if cache_dir is None:
+            cache_dir = tempfile.gettempdir()
+        self.cache_dir = cache_dir
+        
+    def fetch(self, url, max_age=60000):
+        # Use MD5 hash of the URL as the filename
+        filename = hashlib.md5(url).hexdigest()
+        filepath = os.path.join(self.cache_dir, filename)
+        if __name__ != "__main__":
+            filepath = os.path.join('diagnose', filepath)
+            filepath = os.path.join('eduprototype', filepath)
+
+        if os.path.exists(filepath):
+            if int(time.time()) - os.path.getmtime(filepath) < max_age:
+                return open(filepath).read()
+
+        #if cache not found, simply return false
+        return False
+        
+    def cache(self, url, data):
+        # Use MD5 hash of the URL as the filename
+        filename = hashlib.md5(url).hexdigest()
+        filepath = os.path.join(self.cache_dir, filename)
+        if __name__ != "__main__":
+            filepath = os.path.join('diagnose', filepath)
+            filepath = os.path.join('eduprototype', filepath)
+
+        fd, temppath = tempfile.mkstemp()
+        fp = os.fdopen(fd, 'w')
+        fp.write(data.encode("ascii", "ignore"))
+        fp.close()
+        os.rename(temppath, filepath)
+        return data
--- eduwiki/eduprototype/views.py
+++ eduwiki/eduprototype/views.py
@@ -6,71 +6,79 @@ from diagnose.wikipedia import DisambiguationError, page
 from random import randint
 import json
 
-#the index page view
+
+# the index page view
 def index(request):
     context = RequestContext(request)
     context_dict = {}
     return render_to_response('eduprototype/index.html', context_dict, context)
 
-#the intro view
+
+# the intro view
 def quiz(request):
-    if not 'q' in request.GET or not request.GET['q']:
+    if 'q' not in request.GET or not request.GET['q']:
         return redirect('index')
     searchterm = request.GET['q']
     context_dict = {}
     context = RequestContext(request)
     try:
-        json_tree = query(searchterm)  #comes out in a JSON string
+        json_tree = query(searchterm)  # comes out in a JSON string
     except DisambiguationError as dis:
         return disambiguation(request, dis)
-    tree = json.loads(json_tree, object_hook=recurhook)  #converted into a python dictionary
+    # converted into a python dictionary
+    tree = json.loads(json_tree, object_hook=recurhook)
     request.session['tree'] = tree
     context_dict['quiz'] = make_quiz(tree)
-    context_dict['tree'] = tree          #save it to context
+    context_dict['tree'] = tree          # save it to context
     return render_to_response('eduprototype/quiz.html', context_dict, context)
+
 
 def disambiguation(request, dis=[]):
     if not dis:
         return redirect('index')
-    pages = [{'title': option, 'text': description, 'link':link} for option, description, link in zip(dis.options, dis.descriptions, dis.links)]
-    context_dict = {'pages' : pages}
+    pages = [{'title': option, 'text': description, 'link': link}
+             for option, description, link in
+             zip(dis.options, dis.descriptions, dis.links)]
+    context_dict = {'pages': pages}
     context = RequestContext(request)
-    return render_to_response('eduprototype/disambiguation.html', context_dict, context)
+    return render_to_response('eduprototype/disambiguation.html',
+                              context_dict, context)
 
-#the learn view, this is displayed after the quiz is completed to display
-#the information the user needs
+
+# the learn view, this is displayed after the quiz is completed to display
+# the information the user needs
 def learn(request):
     context = RequestContext(request)
-    if not 'q0' in request.GET or not request.GET['q0']:
-        return redirect('index')    
-    #get a list of which questions had correct responses
+    if 'q0' not in request.GET or not request.GET['q0']:
+        return redirect('index')
+    # get a list of which questions had correct responses
     responses = quiz_correctness(request)
-    
-    #note: this is weird because it pulls from session vars, maybe fix later
-    #other note: it's also kinda hard-codey
+
+    # note: this is weird because it pulls from session vars, maybe fix later
+    # other note: it's also kinda hard-codey
     tree = request.session['tree']
-    #get the topics of the main subject
+    # get the topics of the main subject
     main_topic = tree['text']
-    subtopics = [child if not correct else None for child, correct in zip(tree['children'], responses)]
-    
-    context_dict = { 'tree' : tree, "subtopics" : subtopics }
+    subtopics = [child if not correct else None for child, correct in
+                 zip(tree['children'], responses)]
+
+    context_dict = {'tree': tree, "subtopics": subtopics}
     return render_to_response('eduprototype/learn.html', context_dict, context)
-    
 
 
-#code for assembling the dicts needed for the quizzes
-
+# code for assembling the dicts needed for the quizzes
 def make_quiz(topic):
-    #goes though the quizzes and returns a list of dicts for the quiz page
+    # goes though the quizzes and returns a list of dicts for the quiz page
     children = topic['children']
     quiz = []
     for i, child in enumerate(children):
         quiz.append(make_question(child, i))
     return quiz
 
+
 def make_question(topic, num):
-    #a whole bunch of ugly code to randomize the answers but keep
-    #track of which one is correct
+    # a whole bunch of ugly code to randomize the answers but keep
+    # track of which one is correct
     answers_prerand = topic['distractors']
     description = topic['description']
     answers_prerand.insert(0, description)
@@ -80,15 +88,15 @@ def make_question(topic, num):
     for i in range(l):
         correct = "incorrect"
         rand = randint(0, len(answers_prerand) - 1)
-        if rand == 0 and found == False:
+        if rand == 0 and not found:
             correct = "correct"
             found = True
-        answers.append({ 'text' : answers_prerand.pop(rand), 'correct' : correct })
-    return { 'title': topic['title'], 'number': num, 'answers' : answers }
+        answers.append({'text': answers_prerand.pop(rand), 'correct': correct})
+    return {'title': topic['title'], 'number': num, 'answers': answers}
 
 
-#really ugly code to take the request to learn and get back a list
-#of booleans representing whether or not the user answered correctly
+# really ugly code to take the request to learn and get back a list
+# of booleans representing whether or not the user answered correctly
 def quiz_correctness(request):
     i = 0
     while ('q'+str(i)) in request.GET:
@@ -101,15 +109,15 @@ def quiz_correctness(request):
         else:
             responses.append(False)
     return responses
-        
 
-#extra code to fix issues with the way json.loads processes this
+
+# extra code to fix issues with the way json.loads processes this
 
 def recurhook(d):
     if d['children']:
-        #d['children'] = json.loads(d['children'], recurhook)
+        # d['children'] = json.loads(d['children'], recurhook)
         children = []
         for child in d['children']:
-            children.append(json.loads(child, object_hook = recurhook))
+            children.append(json.loads(child, object_hook=recurhook))
         d['children'] = children
     return d
