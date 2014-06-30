6112cc8b53be2fb35e63da11bbbaf45d6381b3d7
Androooou <asherman50@gmail.com>
2014-06-30 11:05:56 -0400

    Fixed some errors introduced in refactoring
    Also cleared cache

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ EduWiki is a prototype for an experimental system that creates custom lesson pla
 
 == Tools Used ==
 
-Eduwiki uses Python for its API handling code, Django for its app structure, and Bootstrap 3.0 for its styling. It uses a modified version of the Wikipedia library to wrap the MediaWiki API, Beautiful Soup 4 for some assorted HTML parsing tasks, and the Python Requests library. It is currently in active prototype development and research at Carnegie Mellon University.
+Eduwiki uses Python for its API handling code, Django for its app structure, and Twitter Bootstrap 3.0 for its styling. It uses a modified version of the Wikipedia library to wrap the MediaWiki API, Beautiful Soup 4 for some assorted HTML parsing tasks, and the Python Requests library. It is currently in active prototype development and research at Carnegie Mellon University.
--- eduwiki/eduprototype/diagnose/diagnose.py
+++ eduwiki/eduprototype/diagnose/diagnose.py
@@ -1,4 +1,7 @@
-import os, tempfile, time, hashlib
+import os
+import tempfile
+import time
+import hashlib
 import wikipedia
 import re
 import json
@@ -6,47 +9,48 @@ import sys
 import unicodedata
 
 
-
-#takes a search term and, optionally, a depth and branch factor, and returns a JSON tree
-#representing the term's article, with its prereqs and quiz items as well as those of 
-#the prereqs, and of their prereqs, etc.
-def query(search_term, depth = 1, children = 3):
-    #set a max depth and branching factor as a failsafe
+# takes a search term and, optionally, a depth and branch factor, and returns a
+# JSON tree representing the term's article, with its prereqs and quiz items as
+# well as those of the prereqs, and of their prereqs, etc.
+def query(search_term, depth=1, children=3):
+    # set a max depth and branching factor as a failsafe
     if depth > 6:
         depth = 6
     if children > 6:
         children = 6
-    
-    #get the topic and the names of its prereq links
+
+    # get the topic and the names of its prereq links
     main_topic = WikiEducate(normal(search_term))
     prereqs = main_topic.wikilinks(children)
     topic_name = main_topic.page.title
-    
-    #create a JSON tree which will be recursively built
+
+    # create a JSON tree which will be recursively built
     json_children = []
-    
-    #get topic text, descriptor, and distractors
+
+    # get topic text, descriptor, and distractors
+    # note: I'm referring to the actual string of text as the distractor
     topic_text = main_topic.plainTextSummary(1)
-    description = main_topic.returnWhatIs() #note: I'm referring to the actual
-    distractor_names = main_topic.wikilinks(children)    #string of text as the distractor
+    description = main_topic.returnWhatIs()
+    distractor_names = main_topic.wikilinks(children)
     distractors = []
-    for i in range(0,3):
+    for i in range(0, 3):
         distractor_name = normal(distractor_names[i])
         distractor_obj = WikiEducate(distractor_name)
-        distractor = distractor_obj.returnWhatIs()    
-        distractors.append(distractor) #append dis tractor *vroom*
-    
-    #run for children if depth left
+        distractor = distractor_obj.returnWhatIs()
+        distractors.append(distractor)  # append dis tractor *vroom*
+
+    # run for children if depth left
     if depth != 0:
-        for j in range(0,children):
-            json_child = query(prereqs[j], depth = depth - 1, children = children)
+        for j in range(0, children):
+            json_child = query(prereqs[j], depth=depth - 1, children=children)
             json_children.append(json_child)
-    
-    #assemble the tree and return it
+
+    # assemble the tree and return it
     json_tree = {'title': topic_name, 'text': topic_text,
-                      'description': description, 'distractors': distractors,
-                      'children': json_children}
+                 'description': description, 'distractors': distractors,
+                 'children': json_children}
     return json.dumps(json_tree)
+
 
 def normal(text):
     try:
@@ -54,11 +58,14 @@ def normal(text):
     except UnicodeEncodeError as u:
         return "ascii"
 
+
 def getpage(topic):
     return WikiEducate(topic)
 
+
 def getpage_exact(topic):
     return WikiEducate(topic, autosuggest=False)
+
 
 class WikiEducate:
     def __init__(self, topic, cache=True, autosuggest=True):
@@ -67,33 +74,34 @@ class WikiEducate:
         self.fetcher = DiskCacheFetcher('url_cache')
         self.page = wikipedia.page(self.topic, auto_suggest=autosuggest)
 
-    
     def wikitext(self):
         text = self.page.wikitext()
         return text
 
     def wikilinks(self, num):
         wtext = self.wikitext()
-        #print wtext
+        # print wtext
         wikilink_rx = re.compile(r'\[\[([^|\]]*\|)?([^\]]+)\]\]')
         link_array = []
         for m in wikilink_rx.finditer(wtext):
             if len(link_array) > num:
                 break
             if m.group(1) is not None:
-                if "Image" in m.group(1) or "Template" in m.group(1) or "File" in m.group(1):
+                if "Image" in m.group(1) or "Template" in m.group(1) or \
+                        "File" in m.group(1):
                     continue
                 link_array.append(m.group(1)[:-1])
             else:
-                if "Image" in m.group(2) or "Template" in m.group(2) or "File" in m.group(2):
+                if "Image" in m.group(2) or "Template" in m.group(2) or \
+                        "File" in m.group(2):
                     continue
                 link_array.append(m.group(2))
         return link_array
 
-
     # Returns (up to) first n paragraphs of given Wikipedia article.
     def plainTextSummary(self, n=2):
-        cached = self.cache and self.fetcher.fetch(self.topic+"-plainTextSummary")
+        cached = self.cache and \
+            self.fetcher.fetch(self.topic+"-plainTextSummary")
         if cached:
             page_content = cached
         else:
@@ -122,7 +130,8 @@ class WikiEducate:
         else:
             self.page = wikipedia.page(self.topic)
             categories = self.page.categories
-            self.fetcher.cache(self.topic+"-categories", json.dumps(categories))
+            self.fetcher.cache(self.topic+"-categories",
+                               json.dumps(categories))
         return categories
 
     # Returns first mention in article of the following regex
@@ -133,22 +142,15 @@ class WikiEducate:
             whatis = cached
         else:
             self.page = wikipedia.page(self.topic)
-            #print self.topic + "||" + self.topic[:7] + "||" + self.page.content[:200] + "\n"
             regex_str = '('+self.topic[:5]+'('+self.topic[5:]+')?'+'|'+'('+self.topic[:len(self.topic)-5]+')?'+self.topic[len(self.topic)-5:] + ')' + '(\s[^\.]*(is|was|can be regarded as)|[^,\.]{,15}?,)\s([^\.]+)\.(?=\s)'
-            #print regex_str + "\n"
             mentions = re.findall(regex_str, self.page.content, re.IGNORECASE)
 
-            if len(mentions) > 0:
-                whatis = mentions[0][5]
-                whatis = re.sub(r'.*\sis\s+(.*)$', r'\1', whatis)
-            else:
+            whatis = mentions[0][5]
+            whatis = re.sub(r'.*\sis\s+(.*)$', r'\1', whatis)
+            if not mentions:
                 whatis = "can't find a good description"
             self.fetcher.cache(self.topic+"-whatis", whatis)
         return whatis
-
-
-
-
 
 
 class DiskCacheFetcher:
@@ -157,7 +159,7 @@ class DiskCacheFetcher:
         if cache_dir is None:
             cache_dir = tempfile.gettempdir()
         self.cache_dir = cache_dir
-        
+
     def fetch(self, url, max_age=60000):
         # Use MD5 hash of the URL as the filename
         filename = hashlib.md5(url).hexdigest()
@@ -170,9 +172,9 @@ class DiskCacheFetcher:
             if int(time.time()) - os.path.getmtime(filepath) < max_age:
                 return open(filepath).read()
 
-        #if cache not found, simply return false
+        # if cache not found, simply return false
         return False
-        
+
     def cache(self, url, data):
         # Use MD5 hash of the URL as the filename
         filename = hashlib.md5(url).hexdigest()
