ea1959d2f0f63ee59a57f944b76c0c92fb332037
guoqithu10@gmail.com <guoqithu10@gmail.com>
2015-06-17 15:28:52 -0400

    mturk page/ launching script: first push

--- autoassess/urls.py
+++ autoassess/urls.py
@@ -10,8 +10,8 @@ urlpatterns = patterns('',
     url(r'^quiz/$', views.quiz, name='quiz'),
     url(r'^disambiguation/$', views.disambiguation, name='disambiguation'),
     url(r'^learn/$', views.learn, name='learn'),
-    url(r'^single_question/$', test_views.single_question, name='single_question'),
-    url(r'^question_submit/$', test_views.question_submit, name='question_submit'),
+    url(r'^single_question$', test_views.single_question, name='single_question'),
+    url(r'^question_submit$', test_views.question_submit, name='question_submit'),
 )
 
 if settings.DEBUG:
