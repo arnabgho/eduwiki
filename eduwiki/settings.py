fa8a7965e19253eba9a50f7aa20aafeeb8bd5f36
guoqithu10@gmail.com <guoqithu10@gmail.com>
2015-06-17 01:02:09 -0400

    gunicorn debugging: installed_app:gunicorn

--- eduwiki/settings.py
+++ eduwiki/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# from __future__ import unicode_literals
+
 import os
 from mongoengine import connect
+
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
