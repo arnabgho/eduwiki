c9e4768580a58e46e735b7bdfad8606310e6ffee
guoqithu10@gmail.com <guoqithu10@gmail.com>
2015-06-21 21:50:15 -0400

    mongoadmin

---
+++ autoassess/mongoadmin.py
@@ -0,0 +1,11 @@
+__author__ = 'moonkey'
+
+from mongonaut.sites import MongoAdmin
+
+# Import your custom models
+from .models import *
+
+
+WikiQuestion.mongoadmin = MongoAdmin()
+WikiQuestionAnswer.mongoadmin = MongoAdmin()
+Prereq.mongoadmin = MongoAdmin()
--- eduwiki/urls.py
+++ eduwiki/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^mongonaut/', include('mongonaut.urls')),
 
     url(r'^$', include('autoassess.urls')),
     url(r'^autoassess/', include('autoassess.urls')),
