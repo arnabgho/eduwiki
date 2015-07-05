ef03e5c8a6747dae4d0ba147116e4200259ddee8
guoqithu10@gmail.com <guoqithu10@gmail.com>
2015-07-05 06:01:51 -0400

    category parsing fix.

--- autoassess/diagnose/util/remove_pages_local.py
+++ autoassess/diagnose/util/remove_pages_local.py
@@ -1,34 +1,35 @@
-__author__ = 'moonkey'
-
-import wikipedia_util
-
-def remove_pages(filename, topic_max=200):
-    topics = []
-    with open(filename, "rU") as topic_file:
-        topic_num = 0
-        cat_line = False
-        for t in topic_file.readlines():
-            if topic_num >= topic_max:
-                break
-            if cat_line:
-                cat_line = False
-                print "Cat:" + t
-                continue
-
-            t = t.strip('\n')
-            if t.replace(" ", "") != "":
-                topics.append(t)
-                topic_num += 1
-            else:
-                cat_line = True
-
-    for t in topics:
-        print t
-        print wikipedia_util.remove_article(t)
-
-
-if __name__ == "__main__":
-    from mongoengine import connect
-    connect('eduwiki_db', host='localhost')
-
-    remove_pages("./no_distractor_topics.txt")
+# __author__ = 'moonkey'
+#
+#
+# def remove_pages(filename, topic_max=200):
+#     import wikipedia_util
+#     topics = []
+#     with open(filename, "rU") as topic_file:
+#         topic_num = 0
+#         cat_line = False
+#         for t in topic_file.readlines():
+#             if topic_num >= topic_max:
+#                 break
+#             if cat_line:
+#                 cat_line = False
+#                 print "Cat:" + t
+#                 continue
+#
+#             t = t.strip('\n')
+#             if t.replace(" ", "") != "":
+#                 topics.append(t)
+#                 topic_num += 1
+#             else:
+#                 cat_line = True
+#
+#     for t in topics:
+#         print t
+#         print wikipedia_util.remove_article(t)
+#
+#
+# if __name__ == "__main__":
+#     from mongoengine import connect
+#
+#     connect('eduwiki_db', host='localhost')
+#
+#     remove_pages("./no_distractor_topics.txt")
