a5b96cc23c31647321908d639d25d8db6a7b91b7
guoqithu10@gmail.com <guoqithu10@gmail.com>
2015-11-05 16:23:15 -0500

    sentence trying limit for distractor generation.

--- autoassess/diagnose/quesgen/distractor/distractor_simcat.py
+++ autoassess/diagnose/quesgen/distractor/distractor_simcat.py
@@ -8,7 +8,15 @@ from autoassess.diagnose.util.NLPU.doc_sim import skip_thoughts_n_sim, \
     sort_docs_by_similarity
 
 
-def generate_distractors_simcat(wikipage, tenses=[], max_num=3):
+def generate_distractors_simcat(wikipage, tenses=[], max_num=3, try_max_num=4):
+    """
+    :param wikipage:
+    :param tenses:
+    :param max_num:
+    :param try_max_num: Try (try_max_num) article sentences to generate
+            a distractor, if fail, then discard the article, to save time
+    :return:
+    """
     distractors = []
 
     for p_title in similar_page_titles_of_samecat(wikipage):
@@ -16,9 +24,14 @@ def generate_distractors_simcat(wikipage, tenses=[], max_num=3):
             sim_page = WikipediaWrapper.page(title=p_title)
             sentences = topic_mentioning_sentence_generator(sim_page)
             distractor = None
+            try_num = 0
             for sentence in sentences:
+                try_num += 1
+                if try_num > try_max_num:
+                    break # give up this page
                 distractor = distractor_from_single_sentence(
-                    sentence, sim_page.title, tenses, original_topic=wikipage.title)
+                    sentence, sim_page.title, tenses,
+                    original_topic=wikipage.title)
                 if distractor:
                     break
 
@@ -27,7 +40,8 @@ def generate_distractors_simcat(wikipage, tenses=[], max_num=3):
                 if len(distractors) >= max_num:
                     break
         except Exception:
-            # There might be page error, or a distractor cannot be generated from the page.
+            # There might be page error,
+            # or a distractor cannot be generated from the page.
             #TODO::logging
             continue
     return distractors
